23a2c56613c5424db137038d46bac3ef56a3b5da
이승구 <leesg107@naver.com>
2025-01-03 16:21:35 +0900

    feat: spring boot 디펜던시 추가 및 초기 세팅

--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     id("java")
-    id("org.springframework.boot") version "3.2.5"
+    id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.5"
 }
+val springCloudVersion by extra("2024.0.0")
 
 group = "com.emotionalcart"
 version = "0.0.1-SNAPSHOT"
@@ -12,9 +13,40 @@ repositories {
 }
 
 dependencies {
+
     implementation(project(":common"))
-    testImplementation(platform("org.junit:junit-bom"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.flywaydb:flyway-mysql")
+    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+    annotationProcessor("org.projectlombok:lombok")
+    // https://mvnrepository.com/artifact/org.projectlombok/lombok
+    compileOnly("org.projectlombok:lombok")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:mysql")
+
+    // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+
+}
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+    }
 }
 
 tasks.test {
