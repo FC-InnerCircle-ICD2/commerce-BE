655f02360b314d1ed5f98d41b0a7f1fb3120a1de
cyeji <choyeji1591@gmail.com>
2025-01-04 15:25:26 +0900

    refactor: responseEntity로 변경

--- order/src/main/java/com/emotionalcart/order/presentation/util/ApiUtil.java
+++ order/src/main/java/com/emotionalcart/order/presentation/util/ApiUtil.java
@@ -1,34 +1,56 @@
 package com.emotionalcart.order.presentation.util;
 
-import lombok.Data;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+/**
+ * API 응답 생성을 위한 유틸리티 클래스
+ */
 public class ApiUtil {
 
-    public static <T> ApiResult<T> ok(T data) {
-        return new ApiResult<>(HttpStatus.OK, data);
+    /**
+     * 성공 응답 생성
+     *
+     * @param <T>    응답 데이터 타입
+     * @param body   응답 본문 데이터
+     * @param status HTTP 상태 코드
+     * @return ResponseEntity<T> 성공 응답
+     */
+    public static <T> ResponseEntity<T> createResponse(T body, HttpStatus status) {
+        return new ResponseEntity<>(body, status);
     }
 
-    public static <T> ApiResult<T> error(HttpStatus httpStatus, String errorMessage) {
-        return new ApiResult<>(httpStatus.value(), errorMessage);
+    /**
+     * 에러 응답 생성
+     *
+     * @param status       HTTP 상태 코드
+     * @param errorMessage 에러 메시지
+     * @return ResponseEntity<ErrorResponse> 에러 응답
+     */
+    public static ResponseEntity<ErrorResponse> error(HttpStatus status, String errorMessage) {
+        ErrorResponse errorResponse = new ErrorResponse(status.value(), errorMessage);
+        return new ResponseEntity<>(errorResponse, status);
     }
 
-    @Data
-    public static class ApiResult<T> {
+    /**
+     * 에러 응답용 객체
+     */
+    public static class ErrorResponse {
 
-        private HttpStatus status;
-        private T response;
-        private int errorCode;
-        private String errorMessage;
+        private final int errorCode;
+        private final String errorMessage;
 
-        private ApiResult(HttpStatus status, T response) {
-            this.status = status;
-            this.response = response;
-        }
-
-        private ApiResult(int errorCode, String errorMessage) {
+        public ErrorResponse(int errorCode, String errorMessage) {
             this.errorCode = errorCode;
             this.errorMessage = errorMessage;
+        }
+
+        public int getErrorCode() {
+            return errorCode;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
         }
 
     }
