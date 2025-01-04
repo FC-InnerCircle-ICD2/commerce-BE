c5a38362251626eb22c3eeb582243109d3e29497
cyeji <choyeji1591@gmail.com>
2025-01-04 14:42:10 +0900

    refactor: API 결과 값의 key를 'data'에서 'response'로 변경

--- order/src/main/java/com/emotionalcart/order/presentation/util/ApiUtil.java
+++ order/src/main/java/com/emotionalcart/order/presentation/util/ApiUtil.java
@@ -5,29 +5,32 @@ import org.springframework.http.HttpStatus;
 
 public class ApiUtil {
 
-    public static <T> ApiResult<T> ok(T data){
+    public static <T> ApiResult<T> ok(T data) {
         return new ApiResult<>(HttpStatus.OK, data);
     }
 
-    public static <T> ApiResult<T> error(HttpStatus httpStatus, String errorMessage){
+    public static <T> ApiResult<T> error(HttpStatus httpStatus, String errorMessage) {
         return new ApiResult<>(httpStatus.value(), errorMessage);
     }
 
     @Data
-    public static class ApiResult<T>{
+    public static class ApiResult<T> {
+
         private HttpStatus status;
-        private T data;
+        private T response;
         private int errorCode;
         private String errorMessage;
 
-         private ApiResult(HttpStatus status, T data) {
+        private ApiResult(HttpStatus status, T response) {
             this.status = status;
-            this.data = data;
+            this.response = response;
         }
 
-        private ApiResult(int errorCode, String errorMessage){
+        private ApiResult(int errorCode, String errorMessage) {
             this.errorCode = errorCode;
             this.errorMessage = errorMessage;
         }
+
     }
+
 }
