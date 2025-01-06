b82752706b83e2a5ac939d0c4183d3fd6e6c0e0b
kimsolhee <solsol6500@naver.com>
2025-01-06 22:44:18 +0900

    - BaseEntity 추가, JpaAuditingConfig 추가, Member phone 컬럼 패턴 체크 부분 제거

--- member/src/main/java/com/emotionalcart/member/domain/Address.java
+++ member/src/main/java/com/emotionalcart/member/domain/Address.java
@@ -1,24 +1,18 @@
-package com.emotionalcart.address.domain;
+package com.emotionalcart.member.domain;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Table
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Address {
+public class Address extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -47,10 +41,4 @@ public class Address {
 
     private Boolean isDefault = false;
 
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 }
--- member/src/main/java/com/emotionalcart/member/domain/Member.java
+++ member/src/main/java/com/emotionalcart/member/domain/Member.java
@@ -3,24 +3,18 @@ package com.emotionalcart.member.domain;
 import com.emotionalcart.member.domain.enums.SocialType;
 import com.emotionalcart.member.domain.enums.UserState;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import java.time.LocalDateTime;
 
 @Entity
 @Table
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,7 +30,6 @@ public class Member {
     private String nickName;
 
     @Column(nullable = false)
-    @Pattern(regexp = "^[0-9]+$", message = "전화번호는 숫자만 포함해야 합니다.")
     private String phone;
 
     @Enumerated(EnumType.STRING)
@@ -46,12 +39,5 @@ public class Member {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private UserState userState;
-
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 
 }
