5c3028b9c365d67046047878754663347a143f5b
kimsolhee <solsol6500@naver.com>
2025-01-10 12:22:26 +0900

    ErrorResponse 필드명 통일, 세션을 Stateless로 설정

--- member/src/main/java/com/emotionalcart/member/exception/ErrorResponse.java
+++ member/src/main/java/com/emotionalcart/member/exception/ErrorResponse.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class ErrorResponse {
-    private final String code;
-    private final String message;
+    private final String errorCode;
+    private final String errorMessage;
 }
--- member/src/main/java/com/emotionalcart/member/infrasturcture/config/SecurityConfig.java
+++ member/src/main/java/com/emotionalcart/member/infrasturcture/config/SecurityConfig.java
@@ -1,9 +1,10 @@
-package com.emotionalcart.member.config;
+package com.emotionalcart.member.infrasturcture.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,25 +16,18 @@ public class SecurityConfig {
                 // CSRF 비활성화 (REST API를 위한 경우)
                 .csrf(AbstractHttpConfigurer::disable)
 
+                // 세션을 Stateless로 설정
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
                 // 권한 및 인증 설정
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // 모든 요청 인증 없이 접근 허용
 //                        .requestMatchers("/", "/login", "/css/**", "/js/**", "/images/**").permitAll()
 //                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
-                )
-
-                // OAuth2 소셜 로그인 활성화
-                .oauth2Login(oauth -> oauth
-                        .loginPage("/login") // 커스텀 로그인 페이지 URL
-                        .defaultSuccessUrl("/") // 로그인 성공 시 이동할 URL
-                        .failureUrl("/login?error") // 로그인 실패 시 이동할 URL
-                )
-
-                // 로그아웃 설정
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/") // 로그아웃 성공 시 이동할 URL
-                        .invalidateHttpSession(true) // 세션 초기화
                 );
+
 
         return http.build();
     }
