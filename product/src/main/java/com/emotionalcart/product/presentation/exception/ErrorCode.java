59dbc19d72bacc415b3f2ee5a2cb90b17aeb9545
Carnival77 <stam0325@gmail.com>
2025-01-11 20:24:04 +0900

    feat : 공통 응답 유틸 생성 (response, pagination, errorcode)

--- product/src/main/java/com/emotionalcart/product/presentation/exception/ErrorCode.java
+++ product/src/main/java/com/emotionalcart/product/presentation/exception/ErrorCode.java
@@ -5,7 +5,14 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    INTERNAL_SERVER_ERROR("PRODUCT-0001", "알 수 없는 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+    INTERNAL_SERVER_ERROR("PRODUCT-0001", "알 수 없는 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR)
+    , INVALID_REQUEST("PRODUCT-0005","잘못된 요청입니다.",HttpStatus.BAD_REQUEST)
+    , UNAUTHORIZE_ERROR("PRODUCT-0002","인증에 실패했습니다.",HttpStatus.UNAUTHORIZED)
+//    , FORBIDDEN(403, "허용되지 않은 접근입니다.")
+    , DATA_NOT_FOUND("PRODUCT-0003","데이터를 찾을 수 없습니다.",HttpStatus.NOT_FOUND)
+//    , BASEURL_BAD_REQUEST(600, "기본 주소 오류입니다.")
+//    , BAD_PARSING(700,"JSON 파일 파싱 에러입니다.")
+    ;
 
     private final String errorCode;
     private final String message;
