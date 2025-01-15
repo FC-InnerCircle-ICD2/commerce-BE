2c8d35eef6a9b06ad83ce973654608e8a7759cd5
yesjm <yesjm.dev@gmail.com>
2025-01-16 01:02:06 +0900

    feat: HandlerMethodValidationException 추가

--- product/src/main/java/com/emotionalcart/core/exception/ErrorCode.java
+++ product/src/main/java/com/emotionalcart/core/exception/ErrorCode.java
@@ -6,8 +6,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR("PRODUCT-0001", "알 수 없는 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    BAD_REQUEST("PRODUCT-0002", "요청에 필요한 값이 비어 있거나 올바르지 않습니다.", HttpStatus.BAD_REQUEST),
 
-    NOT_FOUND_PRODUCT("PRODUCT-0006", "해당 상품을 찾을 수 없습니다.", HttpStatus.NOT_FOUND)
+    NOT_FOUND_PRODUCT("PRODUCT-0006", "해당 상품을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_FOUND_PRODUCT_STOCK("PRODUCT-0007", "해당 옵션에 해당하는 상품을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+
     ;
 
     private final String errorCode;
