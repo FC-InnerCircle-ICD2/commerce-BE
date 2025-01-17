1c1021083ddc0bf29113aa939a81bc57e6eafc9f
song.minji1 <song.minji1@joins.com>
2025-01-17 14:54:36 +0900

    refactor: ProductBannerImage 관련 repository, provider 삭제

--- product/src/main/java/com/emotionalcart/product/domain/BannerDataProvider.java
+++ product/src/main/java/com/emotionalcart/product/domain/BannerDataProvider.java
@@ -2,9 +2,7 @@ package com.emotionalcart.product.domain;
 
 import com.emotionalcart.core.feature.banner.Banner;
 import com.emotionalcart.core.feature.banner.ProductBanner;
-import com.emotionalcart.core.feature.banner.ProductBannerImage;
 import com.emotionalcart.product.infrastructure.repository.BannerRepository;
-import com.emotionalcart.product.infrastructure.repository.ProductBannerImageRepository;
 import com.emotionalcart.product.infrastructure.repository.ProductBannerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -17,7 +15,6 @@ import java.util.List;
 public class BannerDataProvider {
     private final BannerRepository bannerRepository;
     private final ProductBannerRepository productBannerRepository;
-    private final ProductBannerImageRepository productBannerImageRepository;
 
     public List<Banner> findAllBanners(){
         LocalDateTime now = LocalDateTime.now();
@@ -26,9 +23,5 @@ public class BannerDataProvider {
 
     public ProductBanner findProductBanner(Long BannerId){
         return productBannerRepository.findByBannerIdAndIsDeletedFalse(BannerId);
-    }
-
-    public ProductBannerImage findProductBannerImage(Long productBannerId){
-        return productBannerImageRepository.findByProductBannerIdAndIsDeletedFalse(productBannerId);
     }
 }
