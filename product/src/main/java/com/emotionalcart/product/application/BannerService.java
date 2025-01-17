178d3389ebfb967c67c0f3e52b897602e416627e
song.minji1 <song.minji1@joins.com>
2025-01-17 14:56:41 +0900

    refactor: 일급컬렉션 비즈니스 로직 삭제 -> 서비스에서 처리

--- product/src/main/java/com/emotionalcart/product/application/BannerService.java
+++ product/src/main/java/com/emotionalcart/product/application/BannerService.java
@@ -1,8 +1,10 @@
 package com.emotionalcart.product.application;
 
 import com.emotionalcart.core.feature.banner.Banner;
+import com.emotionalcart.core.feature.banner.ProductBanner;
 import com.emotionalcart.product.presentation.dto.ReadBanners;
 import com.emotionalcart.product.domain.BannerDataProvider;
+import com.emotionalcart.product.presentation.dto.ReadProductBanners;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,34 @@ public class BannerService {
     public List<ReadBanners.Response> getBanners(){
         List<Banner> banners = bannerDataProvider.findAllBanners();
 
-        return Banners.from(banners, bannerDataProvider).getBanners();
+        return banners.stream()
+                .map(this::mapBannerToResponse)
+                .toList();
+    }
+
+    private ReadBanners.Response mapBannerToResponse(Banner banner) {
+
+        ReadBanners.Response response = ReadBanners.Response.toResponse(banner, banner.getBannerImage());
+
+        return switch (banner.getType()) {
+            case PRODUCT -> fromProductBanner(response);   // 상품 배너 처리
+            //case EVENT -> fromEventBanner(response);     // 이벤트 배너 처리
+            default -> response;                           // 기본 배너 처리
+        };
+    }
+
+    private ReadBanners.Response fromProductBanner(ReadBanners.Response response) {
+
+        // 상품 배너 데이터 조회
+        ProductBanner productBanner = bannerDataProvider.findProductBanner(response.getId());
+
+        // productBanner가 null인 경우 처리
+        if (productBanner == null) {
+            return response;
+        }
+
+        ReadProductBanners.Response productResponse = ReadProductBanners.Response.toResponse(productBanner);
+
+        return new ReadBanners.Response(response, productResponse);
     }
 }
