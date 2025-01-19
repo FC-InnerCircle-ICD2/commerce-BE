2ef9a6ab3c04479a0694c3fe7e0b295baa7c222c
cyeji <choyeji1591@gmail.com>
2025-01-19 21:37:04 +0900

    Revert "feat : 주문 정보 조회 API 추가"
    This reverts commit cf8227752e5ccacf476b967ce908f2273abd4272.

--- order/src/main/java/com/emotionalcart/order/domain/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emotionalcart.order.domain.repository;
-
-import com.emotionalcart.order.domain.dto.OrderInfo;
-
-public interface OrderRepository {
-
-    OrderInfo findByOrderId(Long id);
-
-}
