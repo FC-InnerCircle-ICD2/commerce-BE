06220677353d27c63daed43dc2ea24191d239a48
cyeji <choyeji1591@gmail.com>
2025-01-19 22:03:30 +0900

    feat : 주문 정보 조회 API 추가

---
+++ order/src/main/java/com/emotionalcart/order/domain/dto/GetOrder.java
@@ -0,0 +1,66 @@
+package com.emotionalcart.order.domain.dto;
+
+import com.emotionalcart.order.domain.entity.OrderRecipient;
+import com.emotionalcart.order.domain.entity.Orders;
+import com.emotionalcart.order.domain.enums.PaymentMethod;
+import lombok.Getter;
+
+/**
+ * 조회된 주문
+ */
+@Getter
+public class GetOrder {
+
+    /**
+     * 주문번호
+     */
+    private final Long orderId;
+
+    /**
+     * 결제수단
+     */
+    private final PaymentMethod paymentMethod;
+
+    /**
+     * 조회된 배송정보
+     */
+    private final GetDeliveryInfo deliveryInfo;
+
+    public GetOrder(Orders orders) {
+        this.orderId = orders.getId();
+        this.paymentMethod = orders.getPaymentMethod();
+        this.deliveryInfo = GetDeliveryInfo.from(orders.getOrderRecipient());
+    }
+
+    public static GetOrder from(Orders orders) {
+        return new GetOrder(orders);
+    }
+
+    @Getter
+    public static class GetDeliveryInfo {
+
+        /**
+         * 수령인 이름
+         */
+        private String recipientName;
+        /**
+         * 수령인 전화번호
+         */
+        private String recipientPhone;
+        /**
+         * 배송지 주소
+         */
+        private String recipientAddress;
+
+        public static GetDeliveryInfo from(OrderRecipient recipient) {
+            GetDeliveryInfo getDeliveryInfo = new GetDeliveryInfo();
+            getDeliveryInfo.recipientName = recipient.getRecipientName();
+            getDeliveryInfo.recipientPhone = recipient.getRecipientPhone();
+            getDeliveryInfo.recipientAddress = recipient.getMappedAddressInfo();
+
+            return getDeliveryInfo;
+        }
+
+    }
+
+}
--- order/src/main/java/com/emotionalcart/order/presentation/controller/OrderController.java
+++ order/src/main/java/com/emotionalcart/order/presentation/controller/OrderController.java
@@ -1,16 +1,16 @@
 package com.emotionalcart.order.presentation.controller;
 
 import com.emotionalcart.order.application.CreateOrderService;
+import com.emotionalcart.order.application.service.GetOrderService;
 import com.emotionalcart.order.domain.dto.CreatedOrder;
+import com.emotionalcart.order.domain.dto.GetOrder;
 import com.emotionalcart.order.presentation.controller.request.CreateOrderRequest;
 import com.emotionalcart.order.presentation.controller.response.CreatedOrderResponse;
+import com.emotionalcart.order.presentation.controller.response.GetOrderResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -19,11 +19,25 @@ public class OrderController {
 
     private final CreateOrderService createOrderService;
 
+    private final GetOrderService getOrderService;
+
     @PostMapping
     public ResponseEntity<CreatedOrderResponse> createOrder(@RequestBody @Valid CreateOrderRequest request) {
         CreatedOrder createdOrder = createOrderService.createOrder(request.mapToDomain());
         // 주문 생성
         return ResponseEntity.ok(CreatedOrderResponse.from(createdOrder));
+    }
+
+    /**
+     * 주문 조회
+     *
+     * @param orderId 주문번호
+     * @return
+     */
+    @GetMapping
+    public ResponseEntity<GetOrderResponse> getOrder(@RequestParam Long orderId) {
+        GetOrder getOrder = getOrderService.getOrder(orderId);
+        return ResponseEntity.ok(GetOrderResponse.from(getOrder));
     }
 
 }
