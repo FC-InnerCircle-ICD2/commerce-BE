d05edc934a6fd05cc5cc16d4a2a9ed97763f1954
cyeji <choyeji1591@gmail.com>
2025-01-19 21:37:04 +0900

    Revert "feat : 주문 정보 조회 API 추가"
    This reverts commit cf8227752e5ccacf476b967ce908f2273abd4272.

--- order/src/main/java/com/emotionalcart/order/domain/entity/Orders.java
+++ order/src/main/java/com/emotionalcart/order/domain/entity/Orders.java
@@ -87,16 +87,59 @@ public class Orders extends BaseEntity {
     @OneToOne(mappedBy = "orders", cascade = CascadeType.ALL, orphanRemoval = true)
     private OrderRecipient orderRecipient;
 
-    public String getRecipientName() {
-        return this.orderRecipient.getRecipientName();
+    /**
+     * <h2>주문 엔티티 생성</h2>
+     * 주문 생성 요청 객체를 받아서 주문 엔티티를 생성한다.<br/>
+     * 주문의 총 합계는 주문 항목의 금액과 수량을 곱한 금액의 합이다.{@link Money#sum(List)}<br/>
+     *
+     * @param createOrder 주문 생성 요청 객체
+     * @return 주문 엔티티
+     */
+    public static Orders createOrder(CreateOrder createOrder) {
+        Orders orders = new Orders();
+        orders.orderAt = LocalDateTime.now();
+        orders.paymentMethod = createOrder.getPaymentMethod();
+        orders.totalPrice =
+            Money.sum(createOrder.getOrderItemsPriceAndQuantity());
+        orders.createOrderItems(createOrder.getOrderItems());
+        orders.createOrderRecipient(createOrder.getDeliveryInfo());
+        return orders;
     }
 
-    public String getRecipientPhone() {
-        return this.orderRecipient.getRecipientPhone();
+    private void createOrderRecipient(@NotNull(message = "배송 정보를 입력해주세요.") DeliveryInfo deliveryInfo) {
+        this.orderRecipient = OrderRecipient.createOrderRecipient(this, deliveryInfo);
     }
 
-    public OrderAddress getRecipientAddress() {
-        return this.orderRecipient.getAddress();
+    public static Orders defaultOrder() {
+        return new Orders();
+    }
+
+    private void createOrderItems(List<CreateOrderItem> orderItems) {
+        this.orderItems = orderItems.stream().map(orderItem -> OrderItem.createOrderItem(this, orderItem)).toList();
+    }
+
+    /**
+     * 주문 상태를 결제 요청으로 변경한다.
+     */
+    public void requestPayment() {
+        this.status = OrderStatus.PAYMENT_REQUESTED;
+    }
+
+    /**
+     * 주문 상태를 배송 요청으로 변경한다.
+     */
+    public void requestShipment() {
+        this.status = OrderStatus.SHIP_REQUESTED;
+    }
+
+    public void addHistory() {
+        if (!CollectionUtils.isEmpty(this.orderItems)) {
+            this.orderItems.forEach(OrderItem::addHistory);
+        }
+    }
+
+    public double getTotalPrice() {
+        return totalPrice.getAmount();
     }
 
 }
