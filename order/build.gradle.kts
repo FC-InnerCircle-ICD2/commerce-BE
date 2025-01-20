08387467f6b995a44a18aca90a320e47c9364f58
cyeji <choyeji1591@gmail.com>
2025-01-20 21:30:34 +0900

    fix : swagger dependency 변경

--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -38,9 +38,6 @@ dependencies {
     // https://mvnrepository.com/artifact/org.redisson/redisson-spring-boot-starter
     implementation("org.redisson:redisson-spring-boot-starter:$redissonVersion")
 
-    // https://mvnrepository.com/artifact/io.swagger.core.v3/swagger-annotations
-    implementation("io.swagger.core.v3:swagger-annotations:2.2.27")
-
     annotationProcessor("org.projectlombok:lombok")
     // https://mvnrepository.com/artifact/org.projectlombok/lombok
     compileOnly("org.projectlombok:lombok")
@@ -52,6 +49,8 @@ dependencies {
     testImplementation("org.testcontainers:mysql")
     // H2 Database Driver
     implementation("com.h2database:h2:2.2.224")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
 
     // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
