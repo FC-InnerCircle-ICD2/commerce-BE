c8760cc660180d16e82934155cff9257e0221d06
cyeji <choyeji1591@gmail.com>
2025-01-20 20:43:50 +0900

    refactor : 주문 조회 실패에 대한 에러코드 추가

--- order/src/main/java/com/emotionalcart/order/infra/enums/OrderErrorCode.java
+++ order/src/main/java/com/emotionalcart/order/infra/enums/OrderErrorCode.java
@@ -9,18 +9,25 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum OrderErrorCode {
 
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다. 입력값을 확인해주세요."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, OrderHttpStatus.BAD_REQUEST, "잘못된 요청입니다. 입력값을 확인해주세요."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, OrderHttpStatus.INTERNAL_SERVER_ERROR, "서버에서 오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
 
-    NULL_VALUE_ERROR(OrderHttpStatus.NULL_VALUE_ERROR, "NULL 값이 포함되어 있습니다."),
-    RESOURCE_NOT_FOUND(OrderHttpStatus.RESOURCE_NOT_FOUND, "요청하신 리소스를 찾을 수 없습니다.");
+    NULL_VALUE_ERROR(HttpStatus.BAD_REQUEST, OrderHttpStatus.NULL_VALUE_ERROR, "NULL 값이 포함되어 있습니다."),
+    RESOURCE_NOT_FOUND(HttpStatus.BAD_REQUEST, OrderHttpStatus.RESOURCE_NOT_FOUND, "요청하신 리소스를 찾을 수 없습니다."),
+    INVALID_ORDER(HttpStatus.BAD_REQUEST, OrderHttpStatus.INVALID_ORDER, "유효하지 않은 주문입니다.");
 
-    private final Enum<?> status;
+    private final HttpStatus status;
+    private final OrderHttpStatus code;
     private final String message;
 
-    OrderErrorCode(Enum<?> status, String message) {
+    OrderErrorCode(HttpStatus status, OrderHttpStatus code, String message) {
         this.status = status;
+        this.code = code;
         this.message = message;
+    }
+
+    public String getErrorCode() {
+        return this.code.getCode();
     }
 
 }
--- order/src/main/java/com/emotionalcart/order/infra/enums/OrderHttpStatus.java
+++ order/src/main/java/com/emotionalcart/order/infra/enums/OrderHttpStatus.java
@@ -8,16 +8,19 @@ import lombok.Getter;
 @Getter
 public enum OrderHttpStatus {
 
-    NULL_VALUE_ERROR(5000),
-    RESOURCE_NOT_FOUND(5001);
+    NULL_VALUE_ERROR("ORDER-0001"),
+    RESOURCE_NOT_FOUND("ORDER-0002"),
+    BAD_REQUEST("ORDER-0003"),
+    INTERNAL_SERVER_ERROR("ORDER-0004"),
+    INVALID_ORDER("ORDER-0005");
 
-    private final int code;
+    private final String code;
 
-    OrderHttpStatus(int code) {
+    OrderHttpStatus(String code) {
         this.code = code;
     }
 
-    public int value() {
+    public String value() {
         return code;
     }
 }
