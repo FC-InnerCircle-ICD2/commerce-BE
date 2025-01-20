863304faa9c99d8d95a38ed9a0dc0ba550b1c543
cyeji <choyeji1591@gmail.com>
2025-01-20 21:08:39 +0900

    refactor : 응답 구조 통일

--- order/src/main/java/com/emotionalcart/order/infra/enums/OrderErrorCode.java
+++ order/src/main/java/com/emotionalcart/order/infra/enums/OrderErrorCode.java
@@ -1,7 +1,6 @@
 package com.emotionalcart.order.infra.enums;
 
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 /**
  * 주문 에러 코드
@@ -9,19 +8,17 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum OrderErrorCode {
 
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, OrderHttpStatus.BAD_REQUEST, "잘못된 요청입니다. 입력값을 확인해주세요."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, OrderHttpStatus.INTERNAL_SERVER_ERROR, "서버에서 오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
+    BAD_REQUEST(OrderHttpStatus.BAD_REQUEST, "잘못된 요청입니다. 입력값을 확인해주세요."),
+    INTERNAL_SERVER_ERROR(OrderHttpStatus.INTERNAL_SERVER_ERROR, "서버에서 오류가 발생했습니다. 잠시 후 다시 시도해주세요."),
 
-    NULL_VALUE_ERROR(HttpStatus.BAD_REQUEST, OrderHttpStatus.NULL_VALUE_ERROR, "NULL 값이 포함되어 있습니다."),
-    RESOURCE_NOT_FOUND(HttpStatus.BAD_REQUEST, OrderHttpStatus.RESOURCE_NOT_FOUND, "요청하신 리소스를 찾을 수 없습니다."),
-    INVALID_ORDER(HttpStatus.BAD_REQUEST, OrderHttpStatus.INVALID_ORDER, "유효하지 않은 주문입니다.");
+    NULL_VALUE_ERROR(OrderHttpStatus.NULL_VALUE_ERROR, "NULL 값이 포함되어 있습니다."),
+    RESOURCE_NOT_FOUND(OrderHttpStatus.RESOURCE_NOT_FOUND, "요청하신 리소스를 찾을 수 없습니다."),
+    INVALID_ORDER(OrderHttpStatus.INVALID_ORDER, "유효하지 않은 주문입니다.");
 
-    private final HttpStatus status;
     private final OrderHttpStatus code;
     private final String message;
 
-    OrderErrorCode(HttpStatus status, OrderHttpStatus code, String message) {
-        this.status = status;
+    OrderErrorCode(OrderHttpStatus code, String message) {
         this.code = code;
         this.message = message;
     }
