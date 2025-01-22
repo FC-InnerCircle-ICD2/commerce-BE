d3106051eea4ec23657982b39e7ccff6df4cc003
Carnival77 <stam0325@gmail.com>
2025-01-22 21:54:09 +0900

    feat : 배너 에러코드 추가

--- product/src/main/java/com/emotionalcart/core/exception/ErrorCode.java
+++ product/src/main/java/com/emotionalcart/core/exception/ErrorCode.java
@@ -11,14 +11,12 @@ public enum ErrorCode {
     NOT_FOUND_PRODUCT_OPTION("PRODUCT-0007", "해당 상품 옵션을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     INVALID_REQUEST("PRODUCT-0005", "잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
     UNAUTHORIZE_ERROR("PRODUCT-0002", "인증에 실패했습니다.", HttpStatus.UNAUTHORIZED)
-    // , FORBIDDEN(403, "허용되지 않은 접근입니다.")
     , DATA_NOT_FOUND("PRODUCT-0003", "데이터를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    // , BASEURL_BAD_REQUEST(600, "기본 주소 오류입니다.")
-    // , BAD_PARSING(700,"JSON 파일 파싱 에러입니다.")
-    NOT_FOUND_CATEGORY("PRODUCT-0008", "해당 카테고리를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_FOUND_CATEGORY("PRODUCT-0008", "카테고리를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     NOT_FOUND_PROVIDER("PRODUCT-0009", "해당 공급자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     NOT_FOUND_PRODUCT_IMAGE("PRODUCT-0010", "해당 상품 이미지를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
-    NOT_FOUND_PRODUCT_OPTION_DETAIL("PRODUCT-0011", "해당 상품 옵션 상세를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    NOT_FOUND_PRODUCT_OPTION_DETAIL("PRODUCT-0011", "해당 상품 옵션 상세를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    NOT_FOUND_BANNER("PRODUCT-0012", "배너를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String errorCode;
     private final String message;
