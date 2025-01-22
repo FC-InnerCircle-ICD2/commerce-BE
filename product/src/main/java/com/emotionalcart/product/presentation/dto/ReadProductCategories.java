40efcb31fb80eb48a4700b860138b1dbf5d18e10
Carnival77 <stam0325@gmail.com>
2025-01-22 21:54:00 +0900

    feat : 상품 상세 조회 시 카테고리 조회용 DTO 생성

---
+++ product/src/main/java/com/emotionalcart/product/presentation/dto/ReadProductCategories.java
@@ -0,0 +1,44 @@
+package com.emotionalcart.product.presentation.dto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.emotionalcart.core.feature.category.Category;
+
+import lombok.Data;
+
+public class ReadProductCategories {
+
+    @Data
+    public static class Response {
+        private Long id;
+        private String name;
+        private Long parentCategoryId;
+        private List<Response> subCategories;
+
+        // Constructor to map Category to Response
+        public Response(Category category) {
+            this.id = category.getId();
+            this.name = category.getName();
+            this.parentCategoryId = category.getParentCategory() != null ? category.getParentCategory().getId()
+                    : null;
+            this.subCategories = category.getSubCategories().stream()
+                    .filter(Category::getIsActive)
+                    .map(Response::new)
+                    .collect(Collectors.toList());
+        }
+
+        // Converts a list of Category objects to a list of Response objects
+        // Only top-level categories (depth == 1) are included
+        public static List<Response> toResponse(List<Category> categories) {
+            return categories.stream()
+                    .filter(category -> category.getDepth() == 1)
+                    .map(Response::new)
+                    .collect(Collectors.toList());
+        }
+
+        public static Response toResponse(Category category) {
+            return new Response(category);
+        }
+    }
+}
