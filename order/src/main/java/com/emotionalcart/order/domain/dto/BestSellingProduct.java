cb48d32333d3d510f1c8d91478a56ddccd6c024a
cyeji <choyeji1591@gmail.com>
2025-02-03 22:58:39 +0900

    fix : 생성자 추가

--- order/src/main/java/com/emotionalcart/order/domain/dto/BestSellingProduct.java
+++ order/src/main/java/com/emotionalcart/order/domain/dto/BestSellingProduct.java
@@ -1,8 +1,13 @@
 package com.emotionalcart.order.domain.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class BestSellingProduct {
 
     /**
