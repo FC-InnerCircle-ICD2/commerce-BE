a107736e518fb5f009a1dbf4bab129d3411e94a0
조예지 <choyeji1591@gmail.com>
2025-02-04 16:24:03 +0900

    refactor : swagger어노테이션 별도 인터페이스 생성

---
+++ order/src/main/java/com/emotionalcart/order/presentation/controller/OrderApiDocs.java
@@ -0,0 +1,22 @@
+package com.emotionalcart.order.presentation.controller;
+
+import com.emotionalcart.order.presentation.controller.request.CreateOrderRequest;
+import com.emotionalcart.order.presentation.controller.response.CreatedOrderResponse;
+import com.emotionalcart.order.presentation.controller.response.OrderDetailResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "주문", description = "주문 API")
+public interface OrderApiDocs {
+
+    @Operation(summary = "주문하기 API")
+    ResponseEntity<CreatedOrderResponse> createOrder(@RequestBody @Valid CreateOrderRequest request);
+
+    @Operation(summary = "주문조회 API")
+    ResponseEntity<OrderDetailResponse> getOrderDetail(@PathVariable Long orderId);
+
+}
--- order/src/main/java/com/emotionalcart/order/presentation/controller/OrderStatisticsController.java
+++ order/src/main/java/com/emotionalcart/order/presentation/controller/OrderStatisticsController.java
@@ -2,7 +2,6 @@ package com.emotionalcart.order.presentation.controller;
 
 import com.emotionalcart.order.application.service.OrderStatisticsService;
 import com.emotionalcart.order.domain.dto.BestSellingProduct;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,11 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "order", description = "order statistics API")
 @RestController
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
-public class OrderStatisticsController {
+public class OrderStatisticsController implements OrderStatisticsDocs {
 
     private final OrderStatisticsService orderStatisticsService;
 
@@ -30,7 +28,7 @@ public class OrderStatisticsController {
     @GetMapping("/sales-ranking/{categoryId}")
     public ResponseEntity<Page<BestSellingProduct>> getProductRanking(@PathVariable Long categoryId, Pageable request) {
         return ResponseEntity.ok().body(orderStatisticsService.getProductRankingByCategoryId(categoryId,
-                                                                                             request));
+                request));
     }
 
 }
