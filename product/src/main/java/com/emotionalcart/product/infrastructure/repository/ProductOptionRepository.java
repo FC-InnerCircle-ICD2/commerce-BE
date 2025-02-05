e8708d0c66db654e4fe44c707dd21ba64cac1ef3
Carnival77 <stam0325@gmail.com>
2025-02-05 23:08:07 +0900

    hotfix : 상품옵션 조회 시 isRequired 조건 임시 삭제

--- product/src/main/java/com/emotionalcart/product/infrastructure/repository/ProductOptionRepository.java
+++ product/src/main/java/com/emotionalcart/product/infrastructure/repository/ProductOptionRepository.java
@@ -9,5 +9,5 @@ import com.emotionalcart.core.feature.product.ProductOption;
 
 @Repository
 public interface ProductOptionRepository extends JpaRepository<ProductOption, Long> {
-    Optional<List<ProductOption>> findAllByProduct_IdAndIsDeletedIsFalseAndIsRequiredIsTrue(Long productId);
+    Optional<List<ProductOption>> findAllByProduct_IdAndIsDeletedIsFalse(Long productId); // 시연 위해 AndIsRequiredIsTrue 제거. 나중에 실제 데이터에서는 추가
 }
