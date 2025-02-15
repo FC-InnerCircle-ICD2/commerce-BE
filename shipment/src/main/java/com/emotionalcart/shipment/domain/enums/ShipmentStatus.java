05ef812820d23393de6978eaf14c86d03ef3c00a
cyeji <choyeji1591@gmail.com>
2025-02-16 02:15:56 +0900

    refactor : 배송 상태 enum 값 변경

--- shipment/src/main/java/com/emotionalcart/shipment/domain/enums/ShipmentStatus.java
+++ shipment/src/main/java/com/emotionalcart/shipment/domain/enums/ShipmentStatus.java
@@ -5,8 +5,9 @@ import lombok.Getter;
 @Getter
 public enum ShipmentStatus {
     SHIP_REQUESTED("배송 요청됨"),       // 운송장 번호 없음, 출고 대기
-    SHIP_READY("배송 준비중"), // 업체가 해당 배송을 확인 후 처리 상태로 변경
-    IN_TRANSIT("배송 중"),      // 운송장 번호 부여됨
+    PENDING("배송 준비중"), // 업체가 해당 배송을 확인 후 처리 상태로 변경
+    SHIPPED("출고 완료"),
+    DELIVERING("배송 중"),      // 운송장 번호 부여됨
     OUT_FOR_DELIVERY("배송 준비 완료"), // 최종 배송 준비 완료
     DELIVERED("배송 완료"),     // 배송 완료됨
     CANCELLED("배송 취소");    // 배송 취소됨
