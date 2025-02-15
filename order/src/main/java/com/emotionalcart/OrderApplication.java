32d59c3442236095932163b001d339f604713ea2
cyeji <choyeji1591@gmail.com>
2025-02-15 14:07:19 +0900

    fix : 스캔 대상 패키지 제거

--- order/src/main/java/com/emotionalcart/OrderApplication.java
+++ order/src/main/java/com/emotionalcart/OrderApplication.java
@@ -11,7 +11,7 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @EnableFeignClients
 @EnableJpaAuditing
-@SpringBootApplication(scanBasePackages = {"com.emotionalcart.order", "com.emotionalcart.adminorder"})
+@SpringBootApplication(scanBasePackages = {"com.emotionalcart.order"})
 public class OrderApplication {
 
     public static void main(String[] args) {
