54a22f9aefd696aa0ed5614e42ef2c86f1c4abf2
이승구 <leesg107@naver.com>
2025-02-19 22:44:28 +0900

    fix: 인증 모듈 접근 시 인증되지 않은경우 대비 entrypoint 적용

--- auth/src/main/java/com/emotionalcart/core/config/SecurityConfig.java
+++ auth/src/main/java/com/emotionalcart/core/config/SecurityConfig.java
@@ -1,9 +1,8 @@
 package com.emotionalcart.core.config;
 
 import com.emotionalcart.auth.application.CustomOAuth2UserService;
-import com.emotionalcart.core.config.jwt.JWTFilter;
-import com.emotionalcart.core.config.jwt.JWTUtil;
 import com.emotionalcart.auth.presentation.handler.CustomSuccessHandler;
+import com.emotionalcart.common.security.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,39 +19,40 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomSuccessHandler customSuccessHandler;
-    private final JWTUtil jwtUtil;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                // CSRF 비활성화 (REST API를 위한 경우)
-                .csrf(AbstractHttpConfigurer::disable)
+            // CSRF 비활성화 (REST API를 위한 경우)
+            .csrf(AbstractHttpConfigurer::disable)
 
-                //From 로그인 방식 disable
-                .formLogin(AbstractHttpConfigurer::disable)
+            //From 로그인 방식 disable
+            .formLogin(AbstractHttpConfigurer::disable)
 
-                //HTTP Basic 인증 방식 disable
-                .httpBasic(AbstractHttpConfigurer::disable)
+            //HTTP Basic 인증 방식 disable
+            .httpBasic(AbstractHttpConfigurer::disable)
 
-                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+            //oauth2
+            .oauth2Login(oauth2 -> oauth2
+                .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                    .userService(customOAuth2UserService))
+                .successHandler(customSuccessHandler)
+            )
 
-                //oauth2
-                .oauth2Login((oauth2) -> oauth2
-                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService))
-                        .successHandler(customSuccessHandler)
-                )
-
-                //경로별 인가 작업(권한 및 인증 설정
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated())
-
-                // 세션을 Stateless 설정
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+            //경로별 인가 작업(권한 및 인증 설정
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/auth/uri/**", "/oauth2/authorization/**").permitAll()
+                .anyRequest().authenticated())
+            .exceptionHandling(exception -> exception
+                .authenticationEntryPoint(customAuthenticationEntryPoint)
+            )
+            // 세션을 Stateless 설정
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            );
 
         return http.build();
     }
+
 }
