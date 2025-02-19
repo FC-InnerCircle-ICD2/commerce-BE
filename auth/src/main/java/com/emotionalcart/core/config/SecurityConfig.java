3b2e24dbe93cea204ec038e590c50e157982b3b0
yesjm <yesjm.dev@gmail.com>
2025-02-20 00:55:49 +0900

    feat:

--- auth/src/main/java/com/emotionalcart/core/config/SecurityConfig.java
+++ auth/src/main/java/com/emotionalcart/core/config/SecurityConfig.java
@@ -41,9 +41,9 @@ public class SecurityConfig {
             )
 
             //경로별 인가 작업(권한 및 인증 설정
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/auth/uri/**", "/oauth2/authorization/**").permitAll()
-                .anyRequest().authenticated())
+//            .authorizeHttpRequests(auth -> auth
+//                .requestMatchers("/api/v1/auth/uri/**", "/oauth2/authorization/**").permitAll()
+//                .anyRequest().authenticated())
             .exceptionHandling(exception -> exception
                 .authenticationEntryPoint(customAuthenticationEntryPoint)
             )
