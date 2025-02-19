fabc58e7a84b264a8e4aee75f57fe320f10d65dd
cyeji <choyeji1591@gmail.com>
2025-02-19 22:20:53 +0900

    feat : FeignClient 설정 추가

--- product/src/main/java/com/emotionalcart/ProductApplication.java
+++ product/src/main/java/com/emotionalcart/ProductApplication.java
@@ -1,14 +1,17 @@
 package com.emotionalcart;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ProductApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
     }
+
 }
