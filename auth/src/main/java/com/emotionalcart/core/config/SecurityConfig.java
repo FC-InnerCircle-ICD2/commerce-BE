b06da564766eb30f6202f8a3c86c17ab644c2df0
조예지 <choyeji1591@gmail.com>
2025-02-20 11:20:30 +0900

    feat : auth 시큐리티 및 스웨거 추가

--- auth/src/main/java/com/emotionalcart/core/config/SecurityConfig.java
+++ auth/src/main/java/com/emotionalcart/core/config/SecurityConfig.java
@@ -24,34 +24,36 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            // CSRF 비활성화 (REST API를 위한 경우)
-            .csrf(AbstractHttpConfigurer::disable)
+                // CSRF 비활성화 (REST API를 위한 경우)
+                .csrf(AbstractHttpConfigurer::disable)
 
-            //From 로그인 방식 disable
-            .formLogin(AbstractHttpConfigurer::disable)
+                //From 로그인 방식 disable
+                .formLogin(AbstractHttpConfigurer::disable)
 
-            //HTTP Basic 인증 방식 disable
-            .httpBasic(AbstractHttpConfigurer::disable)
+                //HTTP Basic 인증 방식 disable
+                .httpBasic(AbstractHttpConfigurer::disable)
 
-            //oauth2
-            .oauth2Login(oauth2 -> oauth2
-                .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
-                    .userService(customOAuth2UserService))
-                .successHandler(customSuccessHandler)
-            )
+                //oauth2
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)
+                )
 
-            //경로별 인가 작업(권한 및 인증 설정)
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/auth/uri/**", "/oauth2/authorization/**", "/actuator/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .exceptionHandling(exception -> exception
-                .authenticationEntryPoint(customAuthenticationEntryPoint)
-            )
-            // 세션을 Stateless 설정
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            );
+                //경로별 인가 작업(권한 및 인증 설정)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**", "/api/v1/auth/uri/**", "/oauth2/authorization/**", "/actuator/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                )
+                // 세션을 Stateless 설정
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
 
         return http.build();
     }
