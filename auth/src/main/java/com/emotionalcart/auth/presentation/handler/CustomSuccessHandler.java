769ea2f67fdf54d5e819749c4eca282fc19a0711
yesjm <yesjm.dev@gmail.com>
2025-02-21 12:25:23 +0900

    feat: 토큰 쿠키에 저장하도록 수정

--- auth/src/main/java/com/emotionalcart/auth/presentation/handler/CustomSuccessHandler.java
+++ auth/src/main/java/com/emotionalcart/auth/presentation/handler/CustomSuccessHandler.java
@@ -3,6 +3,7 @@ package com.emotionalcart.auth.presentation.handler;
 import com.emotionalcart.auth.domain.CustomOAuth2User;
 import com.emotionalcart.common.jwt.JwtProperties;
 import com.emotionalcart.common.jwt.JwtTokenProvider;
+import com.emotionalcart.core.util.CookieUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -26,9 +27,10 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final JwtProperties jwtProperties;
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws
+        IOException {
         HttpSession session = request.getSession();
-        String redirectUri = (String) session.getAttribute("redirect_uri");
+        String redirectUri = (String)session.getAttribute("redirect_uri");
         session.removeAttribute("redirect_uri"); // 사용 후 제거
 
         log.info("Redirect URI: {}", redirectUri);
@@ -38,7 +40,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         }
 
         // OAuth2User
-        CustomOAuth2User oauth2User = (CustomOAuth2User) authentication.getPrincipal();
+        CustomOAuth2User oauth2User = (CustomOAuth2User)authentication.getPrincipal();
         String username = oauth2User.getName();
         Long userId = oauth2User.getUserId();
 
@@ -48,10 +50,11 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         String refreshToken = jwtTokenProvider.createRefreshToken(userId, username, roles);
 
         // 응답 설정
+        CookieUtil.addCookie(response, "Access-Token", accessToken, 5 * 60);
+        CookieUtil.addCookie(response, "Refresh-Token", refreshToken, 30 * 60);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
-        response.setHeader("Access-Token", accessToken);
-        response.setHeader("Refresh-Token", refreshToken);
         response.sendRedirect(redirectUri);
     }
+
 }
