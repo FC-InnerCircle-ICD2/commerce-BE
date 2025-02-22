c27241f013ec49149d4cbb33f603e26bc5d18270
song.minji1 <song.minji1@joins.com>
2025-02-22 14:30:25 +0900

    feat: 상품검증시 재고검증 추가

--- product/src/main/java/com/emotionalcart/product/infrastructure/stock/StockService.java
+++ product/src/main/java/com/emotionalcart/product/infrastructure/stock/StockService.java
@@ -34,4 +34,13 @@ public class StockService {
     public ResponseEntity<Boolean> validateStock(StockQuantityValidateRequest stockQuantityValidateRequest) {
         return stockFeignClient.getStockQuantities(stockQuantityValidateRequest);
     }
+
+    /**
+     * 상품 재고 검증
+     *
+     * @param stockQuantityValidateRequest 상품 재고 검증 요청
+     */
+    public ResponseEntity<Boolean> validateStock(StockQuantityValidateRequest stockQuantityValidateRequest) {
+        return stockFeignClient.getStockQuantities(stockQuantityValidateRequest);
+    }
 }
