0450b950537bde359c2ed692d5fb1f5e1913e8c5
song.minji1 <song.minji1@joins.com>
2025-02-22 14:30:25 +0900

    feat: 상품검증시 재고검증 추가

--- product/src/main/java/com/emotionalcart/product/infrastructure/stock/StockService.java
+++ product/src/main/java/com/emotionalcart/product/infrastructure/stock/StockService.java
@@ -1,8 +1,10 @@
 package com.emotionalcart.product.infrastructure.stock;
 
 import com.emotionalcart.product.infrastructure.stock.dto.StockQuantitySearchRequest;
+import com.emotionalcart.product.infrastructure.stock.dto.StockQuantityValidateRequest;
 import com.emotionalcart.product.infrastructure.stock.http.StockFeignClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -24,5 +26,14 @@ public class StockService {
         Long productId = stockQuantitySearchRequest.getProductId();
         List<Long> optionIds = stockQuantitySearchRequest.getOptionDetailsIds();
         return stockFeignClient.getStockQuantity(productId, optionIds);
+    }
+
+    /**
+     * 상품 재고 검증
+     *
+     * @param stockQuantityValidateRequest 상품 재고 검증 요청
+     */
+    public ResponseEntity<Boolean> validateStock(StockQuantityValidateRequest stockQuantityValidateRequest) {
+        return stockFeignClient.getStockQuantities(stockQuantityValidateRequest);
     }
 }
