788c97212724f10c873c0b820d43aa935a8c3062
Carnival77 <stam0325@gmail.com>
2025-02-22 17:58:15 +0900

    feat : 장바구니 CRUD 기능 개발(Redis 접근 관련 이슈 해결 필요)

---
+++ product/src/main/java/com/emotionalcart/product/presentation/dto/request/AddCartRequest.java
@@ -0,0 +1,34 @@
+package com.emotionalcart.product.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddCartRequest {
+    // 상품 정보
+    private Long productId;
+    private String productName;
+    private int productPrice;
+
+    // 상품 옵션 정보
+    private Long optionId;
+    private String optionName;
+
+    // 상품 상세 옵션
+    private Long detailOptionId;
+    private String detailOptionValue;
+    private int detailOptionQuantity;
+
+    // 상품 대표 이미지
+    private Long imageId;
+    private String imageUrl;
+
+    // 카테고리
+    private Long categoryId;
+    private String categoryName;
+
+    // 공급자
+    private Long providerId;
+    private String providerName;
+}
