b9373a22d1662814c9434d88462faf141fc285df
song.minji1 <song.minji1@joins.com>
2025-02-22 21:24:04 +0900

    feat: 재고검증 api 연동

--- product/src/main/java/com/emotionalcart/product/infrastructure/stock/http/StockFeignClient.java
+++ product/src/main/java/com/emotionalcart/product/infrastructure/stock/http/StockFeignClient.java
@@ -1,8 +1,12 @@
 package com.emotionalcart.product.infrastructure.stock.http;
 
 import com.emotionalcart.core.config.FeignClientConfig;
+import com.emotionalcart.product.infrastructure.stock.dto.StockQuantityValidateRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -13,4 +17,7 @@ public interface StockFeignClient {
     @GetMapping("/quantity")
     Integer getStockQuantity( @RequestParam("productId") Long productId,
                               @RequestParam("optionDetailsIds") List<Long> optionDetailsIds);
+
+    @PostMapping("/quantity/validate")
+    ResponseEntity<Boolean> getStockQuantities(@RequestBody StockQuantityValidateRequest requests);
 }
