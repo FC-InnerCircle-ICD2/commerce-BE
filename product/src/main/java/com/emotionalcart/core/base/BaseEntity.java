c4dc9c242afa70d1a46544c12fea888301fa9593
yesjm <yesjm.dev@gmail.com>
2025-02-22 17:29:33 +0900

    feat: 상품 삭제 기능 추가

--- product/src/main/java/com/emotionalcart/core/base/BaseEntity.java
+++ product/src/main/java/com/emotionalcart/core/base/BaseEntity.java
@@ -5,6 +5,7 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -23,5 +24,6 @@ public abstract class BaseEntity {
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    @Setter
     private Boolean isDeleted = false;
 }
