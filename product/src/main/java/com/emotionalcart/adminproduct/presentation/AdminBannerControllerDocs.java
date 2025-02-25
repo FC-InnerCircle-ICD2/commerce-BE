87a6319b2b0684cf588b29c191bc672c9aef884f
song.minji1 <song.minji1@joins.com>
2025-02-25 15:03:02 +0900

    feat: 배너 update 기능 추가

--- product/src/main/java/com/emotionalcart/adminproduct/presentation/AdminBannerControllerDocs.java
+++ product/src/main/java/com/emotionalcart/adminproduct/presentation/AdminBannerControllerDocs.java
@@ -1,9 +1,6 @@
 package com.emotionalcart.adminproduct.presentation;
 
-import com.emotionalcart.adminproduct.presentation.dto.CreateBannerRequest;
-import com.emotionalcart.adminproduct.presentation.dto.CreateBannerResponse;
-import com.emotionalcart.adminproduct.presentation.dto.ReadBannerDetailResponse;
-import com.emotionalcart.adminproduct.presentation.dto.ReadBannersResponse;
+import com.emotionalcart.adminproduct.presentation.dto.*;
 import com.emotionalcart.core.exception.GlobalExceptionHandler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -68,4 +65,24 @@ public interface AdminBannerControllerDocs {
 
     @Operation(summary = "배너 삭제")
     ResponseEntity<Void> deleteBanner(@PathVariable Long bannerId);
+
+    @Operation(
+        summary = "배너 수정",
+        description = "기존 배너 정보를 수정합니다.",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "배너 수정 성공",
+                content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ReadBannerDetailResponse.class),
+                    examples = @ExampleObject(name = "배너 수정 성공", value = "{ \"bannerId\": 1 }")
+                )),
+
+            @ApiResponse(responseCode = "400", description = "잘못된 요청 (유효성 검사 실패)",
+                content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = GlobalExceptionHandler.ExceptionResponse.class)))
+        }
+    )
+    ResponseEntity<ReadBannerDetailResponse> updateBanner(
+        @PathVariable Long bannerId,
+        @ModelAttribute @Valid UpdateBannerRequest request
+    );
 }
