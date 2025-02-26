d5b13de47b10c75c5174ec864d66288976798af9
ziwookim <ziwookim@gimjiuui-MacBookPro.local>
2025-02-26 22:24:16 +0900

    fix. 백오피스 판매처 (공급자) 등록 api 오류 수정

--- product/src/main/java/com/emotionalcart/adminproduct/application/AdminProviderService.java
+++ product/src/main/java/com/emotionalcart/adminproduct/application/AdminProviderService.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
+@Transactional
 public class AdminProviderService {
 
     private final AdminProviderDataProvider adminProviderDataProvider;
