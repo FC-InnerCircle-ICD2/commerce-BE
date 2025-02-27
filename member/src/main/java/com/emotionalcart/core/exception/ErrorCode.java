81f96af241567b9f9746ddad6674fb0a534f8272
ziwookim <ziwookim@gimjiuui-MacBookPro.local>
2025-02-28 00:22:28 +0900

    fix. 에러 코드 중복 수정

--- member/src/main/java/com/emotionalcart/core/exception/ErrorCode.java
+++ member/src/main/java/com/emotionalcart/core/exception/ErrorCode.java
@@ -16,7 +16,7 @@ public enum ErrorCode {
 
     // 백오피스
     INVALID_MEMBER_ROLE_VALUE(HttpStatus.BAD_REQUEST, "MEMBER-1000", "memberRole 값에 부적절한 입력 값이 포함되어 있습니다."),
-    INVALID_ADMIN_MEMBER(HttpStatus.BAD_REQUEST, "MEMBER-1000", "존재하지 않는 관리자 계정 입니다.");
+    INVALID_ADMIN_MEMBER(HttpStatus.BAD_REQUEST, "MEMBER-1001", "존재하지 않는 관리자 계정 입니다.");
 
 
 
