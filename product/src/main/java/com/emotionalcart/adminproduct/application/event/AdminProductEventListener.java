e4129272203bc79259637e249ea6167ab5c7495d
yesjm <yesjm.dev@gmail.com>
2025-03-07 00:24:55 +0900

    feat: 상품 생성시 재고 생성 이벤트 발행

--- product/src/main/java/com/emotionalcart/adminproduct/application/event/AdminProductEventListener.java
+++ product/src/main/java/com/emotionalcart/adminproduct/application/event/AdminProductEventListener.java
@@ -2,7 +2,9 @@ package com.emotionalcart.adminproduct.application.event;
 
 import com.emotionalcart.adminproduct.domain.event.ProductCreatedEvent;
 import com.emotionalcart.adminproduct.domain.event.ProductDeletedEvent;
+import com.emotionalcart.adminproduct.domain.event.ProductStockCreateEvent;
 import com.emotionalcart.adminproduct.infrastructure.search.SearchService;
+import com.emotionalcart.product.infrastructure.stock.StockService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -15,6 +17,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class AdminProductEventListener {
 
     private final SearchService searchService;
+    private final StockService stockService;
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleProductCreated(ProductCreatedEvent event) {
@@ -26,5 +29,11 @@ public class AdminProductEventListener {
     public void handleProductDeleted(ProductDeletedEvent event) {
         log.info("ProductDeletedEvent: {}", event);
         searchService.indexDeleteProduct(event);
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleProductStockCreate(ProductStockCreateEvent event) {
+        log.info("ProductStockCreateEvent: {}", event);
+        stockService.generateOptionCombinationsAndSaveStock(event.id());
     }
 }
