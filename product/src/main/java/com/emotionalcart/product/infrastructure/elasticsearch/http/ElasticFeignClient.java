a12a868a41e426a60e41e28f8f2ccd2d0c6f2eb1
song.minji1 <song.minji1@joins.com>
2025-03-07 16:31:56 +0900

    feat: 상품 목록 조회 엘라스틱서치 연동 추가

---
+++ product/src/main/java/com/emotionalcart/product/infrastructure/elasticsearch/http/ElasticFeignClient.java
@@ -0,0 +1,24 @@
+package com.emotionalcart.product.infrastructure.elasticsearch.http;
+
+import com.emotionalcart.core.config.FeignClientConfig;
+import com.emotionalcart.product.infrastructure.elasticsearch.dto.ElasticProduct;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(name = "elastic-service", url = "${elastic.find.feign-endpoint}", path = "/api/v1/elastic", configuration = FeignClientConfig.class)
+public interface ElasticFeignClient {
+
+    @GetMapping("/product")
+    ResponseEntity<List<ElasticProduct>> searchProducts(
+        @RequestParam(value = "keyword", required = false) String keyword,
+        @RequestParam(value = "sortOption", required = false) String sortOption,
+        @RequestParam(value = "page", defaultValue = "1") int page,
+        @RequestParam(value = "size", defaultValue = "10") int size,
+        @RequestParam(value = "minPrice", required = false) Integer minPrice,
+        @RequestParam(value = "maxPrice", required = false) Integer maxPrice
+    );
+}
