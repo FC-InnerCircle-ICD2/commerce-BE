f01eeeb47ee422fdd127008d308000b628927877
leeseunggu <32692807+seunggulee1007@users.noreply.github.com>
2025-03-08 02:40:10 +0900

    Revert "feat : 사용자 주문 검증 후 응답 값 리턴"

--- order/src/main/java/com/emotionalcart/order/presentation/controller/OrderController.java
+++ order/src/main/java/com/emotionalcart/order/presentation/controller/OrderController.java
@@ -10,7 +10,6 @@ import com.emotionalcart.order.presentation.controller.request.CreateOrderReques
 import com.emotionalcart.order.presentation.controller.response.CreatedOrderResponse;
 import com.emotionalcart.order.presentation.controller.response.OrderDetailResponse;
 import com.emotionalcart.order.presentation.controller.response.UserOrderResponse;
-import com.emotionalcart.order.presentation.controller.response.ValidOrderResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -60,10 +59,8 @@ public class OrderController implements OrderApiDocs {
      * 사용자 주문 검증
      */
     @GetMapping("/users/validate/{orderId}")
-    public ResponseEntity<ValidOrderResponse> validateOrderByMember(@AuthenticationPrincipal JwtAuthentication jwt,
-                                                                    @PathVariable Long orderId,
-                                                                    @RequestParam Long productId) {
-        ValidOrderResponse isExistOrder = orderDetailService.validateOrderByMember(jwt.id(), orderId, productId);
+    public ResponseEntity<Boolean> validateOrderByMember(@AuthenticationPrincipal JwtAuthentication jwt, @PathVariable Long orderId) {
+        Boolean isExistOrder = orderDetailService.validateOrderByMember(jwt.id(), orderId);
         return ResponseEntity.ok().body(isExistOrder);
     }
 
