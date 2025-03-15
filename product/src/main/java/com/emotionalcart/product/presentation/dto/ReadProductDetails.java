4d506f2cdceffa1943800aee42121bce0eca6ab7
cyeji <choyeji1591@gmail.com>
2025-03-16 00:37:48 +0900

    fix : 상품 상세 조회 이슈 수정

--- product/src/main/java/com/emotionalcart/product/presentation/dto/ReadProductDetails.java
+++ product/src/main/java/com/emotionalcart/product/presentation/dto/ReadProductDetails.java
@@ -1,19 +1,20 @@
 package com.emotionalcart.product.presentation.dto;
 
-import java.util.List;
-
 import com.emotionalcart.core.feature.product.Product;
-
+import com.emotionalcart.core.feature.product.ProductOption;
 import com.emotionalcart.product.infrastructure.stock.dto.OptionStockResult;
 import com.emotionalcart.product.infrastructure.stock.dto.OptionStocksResponse;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
+import java.util.List;
+
 public class ReadProductDetails {
 
     @Data
     public static class Response {
+
         @JsonSerialize(using = ToStringSerializer.class)
         private Long id;
         private String name;
@@ -28,16 +29,16 @@ public class ReadProductDetails {
         //private int totalStockQuantity;
 
         public Response(Product product,
-                List<ReadProductOptions.Response> options, ReadProductCategories.Response categoryResponse,
-                ReadProviders.Response providerResponse, ReadProductReviewStatistic.Response reviewStatistic,
-                List<ReadProductImages.Response> images, OptionStockResult stockResult) {
+                        List<ProductOption> options, ReadProductCategories.Response categoryResponse,
+                        ReadProviders.Response providerResponse, ReadProductReviewStatistic.Response reviewStatistic,
+                        List<ReadProductImages.Response> images, OptionStockResult stockResult) {
             this.id = product.getId();
             this.name = product.getName();
             this.description = product.getDescription();
             this.price = product.getPrice();
             this.category = categoryResponse;
             this.provider = providerResponse;
-            this.options = options;
+            this.options = ReadProductOptions.Response.from(options);
             this.reviewStatistic = reviewStatistic;
             this.images = images;
             this.optionStocks = stockResult.getOptionStocksResponses();
@@ -45,10 +46,12 @@ public class ReadProductDetails {
         }
 
         public static Response toResponse(Product product,
-                List<ReadProductOptions.Response> options, ReadProductCategories.Response categoryResponse,
-                ReadProviders.Response providerResponse, ReadProductReviewStatistic.Response reviewStatistic,
-                List<ReadProductImages.Response> images, OptionStockResult stockResult) {
+                                          List<ProductOption> options, ReadProductCategories.Response categoryResponse,
+                                          ReadProviders.Response providerResponse, ReadProductReviewStatistic.Response reviewStatistic,
+                                          List<ReadProductImages.Response> images, OptionStockResult stockResult) {
             return new Response(product, options, categoryResponse, providerResponse, reviewStatistic, images, stockResult);
         }
+
     }
+
 }
--- product/src/main/java/com/emotionalcart/product/presentation/dto/ReadProductOptionDetails.java
+++ product/src/main/java/com/emotionalcart/product/presentation/dto/ReadProductOptionDetails.java
@@ -1,15 +1,18 @@
 package com.emotionalcart.product.presentation.dto;
 
 import com.emotionalcart.core.feature.product.ProductOptionDetail;
-
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.util.List;
 
 public class ReadProductOptionDetails {
 
     @Data
     public static class Response {
+
         @JsonSerialize(using = ToStringSerializer.class)
         private Long id;
         private String value;
@@ -23,8 +26,25 @@ public class ReadProductOptionDetails {
             this.additionalPrice = optionDetail.getAdditionalPrice();
         }
 
+        public Response(Long id, @NotNull String value, @NotNull Integer optionOrder, Integer additionalPrice) {
+            this.id = id;
+            this.value = value;
+            this.order = optionOrder;
+            this.additionalPrice = additionalPrice;
+        }
+
         public static Response toResponse(ProductOptionDetail optionDetail) {
             return new Response(optionDetail);
         }
+
+        public static Response from(ProductOptionDetail detail) {
+            return new Response(detail.getId(), detail.getValue(), detail.getOptionOrder(), detail.getAdditionalPrice());
+        }
+
+        public static List<Response> from(List<ProductOptionDetail> details) {
+            return details.stream().map(Response::from).toList();
+        }
+
     }
+
 }
